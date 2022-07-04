8532d8f784df00a6180824892743b5620e573b64
Ouenniche <panambons@gmail.com>
2022-07-04 05:57:21 +0100

    commit

---
+++ Checkpoint/javascript/data.js
@@ -0,0 +1,29 @@
+let shopItemsData = [
+    {
+        id: "first",
+        name: "Casual Shirt",
+        price: 45,
+        desc: "Blue and black gingham checkered shirt by Green Hill The Secret",
+        img: "images/shirt.jpg" ,
+    },
+    {
+        id: "second",
+        name: "Watches",
+        price: 100,
+        desc: "Ce chronographe a été conçu pour être le meilleur outil de chronométrage des pilotes de course d’endurance.",
+        img: "images/watch.jpg" ,
+    },
+    {
+        id: "third",
+        name: "Suits",
+        price: 300,
+        desc: "Elegant Groom's Wear Black Smoking Dinner jacket/Wedding Suits For Men/Best man's 3 Peices Suits.",
+        img: "images/suits.jpg" ,
+    },
+    {
+        id: "fourth",
+        name: "Shoes",
+        price: 115,
+        desc: "Fashion Men Shoes Leather Shoes Men’s Flats Shoes Low men Oxford Shoes",
+        img: "images/shoes.jpg", 
+    }];
